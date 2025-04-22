50565f42500340bdc8536c491f1c0e53910e0de8
Malik Usman <usman003591@gmail.com>
2025-04-22 16:46:58 +0500

    Add project files.

---
+++ Models/Task.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskX.Models
+{
+    public class Task
+    {
+        //[Key] is not necessary here as the Id property is already the primary key by convention
+        public int Id { get; set; } //As the name of the attribute is Id or includes the Id word in it, it will be used as the primary key by default
+
+        [Required]
+        public required string Title { get; set; }
+
+        [MaxLength(200)]
+        public string? Description { get; set; } //Adding a question mark makes this property nullable or you can use this method as well
+
+        //public string Description { get; set; } = string.Empty; //This is another way to make the property nullable. In this method is being initialized with an empty string
+        [Required]
+        public bool Bookmarked { get; set; } = false; //This is the default value for this property
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now; //This is the default value for this property
+
+    }
+}

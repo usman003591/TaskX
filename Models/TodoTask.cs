aa80e56beeb9db8255c0bb89fe18098d59da5419
Malik Usman <usman003591@gmail.com>
2025-05-17 14:12:48 +0500

    added client side and server side validations and counter in the text area of create

--- Models/TodoTask.cs
+++ Models/TodoTask.cs
@@ -4,13 +4,13 @@ namespace TaskX.Models
 {
     public class TodoTask
     {
-        [Key] //is not necessary here as the Id property is already the primary key by convention
+        //[Key] //is not necessary here as the Id property is already the primary key by convention
         public int Id { get; set; } //As the name of the attribute is Id or includes the Id word in it, it will be used as the primary key by default
 
-        [Required]
+        [Required(ErrorMessage ="Please enter a title!")]
         public required string Title { get; set; }
 
-        [MaxLength(200)]
+        [MaxLength(500)]
         public string? Description { get; set; } //Adding a question mark makes this property nullable or you can use this method as well
 
         //public string Description { get; set; } = string.Empty; //This is another way to make the property nullable. In this method is being initialized with an empty string
